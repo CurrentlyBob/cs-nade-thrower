46216afc2b7064790f90b549d66c509f3f9f14b5
CurrentlyBobe <walshbobby5@gmail.com>
2023-04-25 14:32:20 -0500

    index function

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as nadeThrowRouter } from './routes/nade-throws.js'
+import { router as profilesRouter } from './routes/profiles.js'
 // create the express app
 const app = express()
 
@@ -62,6 +63,8 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/nade-throws', nadeThrowRouter)
+app.use('/profiles', profilesRouter)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
