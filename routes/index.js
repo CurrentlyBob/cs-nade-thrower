b7c85df95f35dbb676e9d4668701018e4281b1d3
CurrentlyBobe <walshbobby5@gmail.com>
2023-04-26 08:42:00 -0500

    on sign in redirect

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', function (req, res) {
+  if (req.user) {
+    res.redirect('/nade-throws')
+  } else {
   res.render('index', { title: 'Home Page' })
+  }
 })
 
 export {
